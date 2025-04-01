0fc809c8c68e8eb1bd1be7ee462789d2db746a48
Jayanthkumar04 <jayanthkumar598@gmail.com>
2025-04-01 15:13:07 +0530

    Add project files.

---
+++ SessionService/IService1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SessionService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+         void save(Course course);
+
+        [OperationContract]
+
+        List<Course> getCourses();
+
+
+    }
+
+    [DataContract]
+    public class Course
+    {
+        [DataMember]
+     public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+    }
+
+
+}
---
+++ SessionService/Service1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SessionService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public List<Course> courses;
+        Service1()
+        {
+            courses = new List<Course>();
+
+        }
+        public List<Course> getCourses()
+        {
+            return courses;
+        }
+
+        public void save(Course course)
+        {
+            courses.Add(course);
+        }
+    }
+}
